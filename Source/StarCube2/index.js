5f2cdb41c49d9a5341cc8240235f3077b5f51158
James Hooks <james.hooks@mail.utoronto.ca>
2019-08-02 08:29:40 -0400

    Objects and materials working

--- Source/StarCube2/index.js
+++ Source/StarCube2/index.js
@@ -1,12 +1,12 @@
-var projection = mat4.create()    // projection matrix
-var modelview                    // modelview matrix; value comes from rotator
-var normalMatrix = mat3.create();  // matrix, derived from modelview matrix, for transforming normal vectors
+var projection = mat4.create()  // projection matrix
+var modelview = mat4.create()   // modelview matrix; value comes from rotator
+var normalMatrix = mat3.create()    // matrix, derived from modelview matrix, for transforming normal vectors
 
 var scale = 1
 var dt = 1000/60
 var time = 0
 
-var fieldSize = 100//100000
+var fieldSize = 100000
 var seeds 
 var freqMin = 0.1 
 var freqMax = 4
@@ -33,7 +33,6 @@ var cube = null
 /* Initialize the WebGL context.  Called from init() */
 function init() {
     glInit(() => {
-        gl.enable(gl.DEPTH_TEST)
 
         rotator = new TrackballRotator(canvas, draw, 15)
         zoomer = new Zoomer(canvas) 
@@ -65,19 +64,19 @@ function reset() {
 
     seeds = [Math.random(), Math.random(), Math.random(), Math.random()]
 
+    gl.viewport(0,0, canvas.width, canvas.height)
     /* Create Objects */
-    /*field = initObject({
+    field = initObject({
         name:"unlitPoint",
         elements:fieldSize,
         drawMode: gl.POINTS
-    })*/
-
-    gl.viewport(0,0, canvas.width, canvas.height)
+    })
+    generateField()
 
     var color = defaultColors[0]
     color.push(1)
-    var mat = initMaterial("phongColor", {
-        "specularColor": [0.5, 0.5, 0.5],
+    var mat = initMaterial("phongTexture", {
+        "specularColor": [1, 0, 0],
         "diffuseColor": color,
         "specularExponent": 1,
         "lightPosition": defaultLights[1]
@@ -88,15 +87,11 @@ function reset() {
         model:"cube"
     })
 
-    // generateField()
-    draw()
-
-    /* Start draw field *
+    /* Start draw field */
     texture = gl.createTexture()
     gl.bindTexture(gl.TEXTURE_2D, texture)
     gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, 512, 512, 
     	0, gl.RGBA, gl.UNSIGNED_BYTE, null)
-
 
     gl.viewport(0,0,512, 512)
 
@@ -104,23 +99,16 @@ function reset() {
     gl.bindFramebuffer(gl.FRAMEBUFFER, framebuffer)
     gl.framebufferTexture2D(gl.FRAMEBUFFER, gl.COLOR_ATTACHMENT0, gl.TEXTURE_2D, texture, 0)
 
-    initPoint()
-    generateField()
     drawField()
 
     gl.generateMipmap(gl.TEXTURE_2D);
 
     gl.bindFramebuffer(gl.FRAMEBUFFER, null)
+
+    // Draw mains stuff
     gl.viewport(0,0,canvas.width, canvas.height)
-
-    // drawField()
-
-    // Draw cube now
-    initBasicTexture()
-    installModelWithTexture(objects[0])
-    currentModelNumber = 0
-
     draw()
+
     /* End draw field */
 
     /* Start test texture *
@@ -130,6 +118,7 @@ function reset() {
 
     var image = new Image();
     image.src = './space.jpg'
+    image.crossOrigin = "anonymous"; 
     image.addEventListener('load', function() {
         console.log("LOADED!")
         console.log(image)
@@ -140,12 +129,61 @@ function reset() {
 
         // Draw cube now
         initBasicTexture()
-        installModelWithTexture(objects[0])
-        currentModelNumber = 0
+        // installModelWithTexture(objects[0])
+        // currentModelNumber = 0
 
         draw()
     });
     /* End Test Texture */
+
+    //draw()
+}
+
+function draw() { 
+    gl.clearColor(clearVal, clearVal, clearVal, 1);  // specify the color to be used for clearing
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    // Draw flat field as background
+    mat4.ortho(projection, -1.0, 1.0, -1.0, 1.0, 0.1, 100); 
+    // modelview = rotator.getViewMatrix()//mat4.create()
+    modelview = mat4.create()
+ 
+    gl.disable(gl.DEPTH_TEST)
+
+    if(field) {
+        field.dirtyAttributes()
+        field.draw(modelview, projection, null)
+    }
+
+    gl.enable(gl.DEPTH_TEST) 
+
+    // Draw perspective object
+    mat4.perspective(projection, Math.PI/5, 1, 10, 20);
+    modelview = rotator.getViewMatrix();
+
+    mat4.scale(modelview, modelview, vec3.fromValues(scale, scale, scale))
+    if(zoomer) {
+	    var zoom = zoomer.getZoomScale()
+	    mat4.scale(modelview, modelview, vec3.fromValues(zoom,zoom,zoom))
+	}
+    mat3.normalFromMat4(normalMatrix, modelview);
+
+    if(cube)
+        cube.draw(modelview, projection, normalMatrix)
+}
+
+function drawField() {
+    gl.clearColor(clearVal, clearVal, clearVal, 1);  // specify the color to be used for clearing
+    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT); 
+
+    // Draw flat field as background
+    mat4.ortho(projection, -1.0, 1.0, -1.0, 1.0, 0.1, 100); 
+    modelview = mat4.create()
+ 
+    if(field) {
+        field.dirtyAttributes()
+        field.draw(modelview, projection, null)
+    }
 }
 
 function getNoise(x, y, i) {
@@ -161,7 +199,7 @@ function generateField() {
         // Add random point to canvas
         var randX =  Math.random()
         var randY =  Math.random()
-        var randZ = 0
+        var randZ = -4
 
         var vec = vec3.fromValues(randX, randY, randZ)
         points.push(vec)
@@ -266,50 +304,18 @@ function generateField() {
         field.setValue("coords", i*3 + 1, -1 + 2*points[i][1])
         field.setValue("coords", i*3 + 2, 2*points[i][2])
 
-        // field.setValue("color", i*3 + 0, Math.min(clearVal + lerp(r, r2, f3)*(1-clearVal), 1))
-        // field.setValue("color", i*3 + 1, Math.min(clearVal + lerp(g, g2, f3)*(1-clearVal), 1))
-        // field.setValue("color", i*3 + 2, Math.min(clearVal + lerp(b, b2, f3)*(1-clearVal), 1))
+        field.setValue("color", i*3 + 0, Math.min(clearVal + lerp(r, r2, f3)*(1-clearVal), 1))
+        field.setValue("color", i*3 + 1, Math.min(clearVal + lerp(g, g2, f3)*(1-clearVal), 1))
+        field.setValue("color", i*3 + 2, Math.min(clearVal + lerp(b, b2, f3)*(1-clearVal), 1))
 
-        // field.setValue("size", i, s)
-
-        field.setValue("color", i*3 + 0, 1)
-        field.setValue("color", i*3 + 1, 1)
-        field.setValue("color", i*3 + 2, 1)
-
-        field.setValue("size", i, 5)
- 
-
+        field.setValue("size", i, s)
+        // field.setValue("color", i*3 + 0, 1)
+        // field.setValue("color", i*3 + 1, 1)
+        // field.setValue("color", i*3 + 2, 1)
+        // field.setValue("size", i, 1)
     }
-    console.log(field.getArray("coords"))
-    field.dirty()
-    console.log("Dirty")
+
+    field.dirtyAttributes()
 
     //console.log("Min: " + min + " Max: " + max)
-}
-
-function draw() { 
-    gl.clearColor(clearVal, clearVal, clearVal, 1);  // specify the color to be used for clearing
-    gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-
-    // Draw flat field as background
-    mat4.ortho(projection, -1.0, 1.0, -1.0, 1.0, 0.1, 100); 
-    modelview = mat4.create()
-
-    if(field)
-        field.draw(modelview, projection, null, true)
-
-    // Draw perspective object
-    mat4.perspective(projection, Math.PI/5, 1, 10, 20);
-    modelview = rotator.getViewMatrix();
-
-    mat4.scale(modelview, modelview, vec3.fromValues(scale, scale, scale))
-    if(zoomer) {
-	    var zoom = zoomer.getZoomScale()
-	    mat4.scale(modelview, modelview, vec3.fromValues(zoom,zoom,zoom))
-	}
-    mat3.normalFromMat4(normalMatrix, modelview);
-
-    if(cube)
-        cube.draw(modelview, projection, normalMatrix)
 }
