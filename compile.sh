cd340f5dd3feae505367c0b1e3a2982ad198977f
James Hooks <james.hooks@mail.utoronto.ca>
2020-03-15 16:22:07 -0400

    Updated compiling script

--- compile.sh
+++ compile.sh
@@ -2,5 +2,7 @@ if [ -z "$1" ]; then
 	echo "usage ./compile outputName"
 	exit 1
 fi
-./node_modules/inline-scripts/src/cli/inlineScriptTags.js "./${1}/index.html" "./Play/${1}.html"
+#./node_modules/inline-scripts/src/cli/inlineScriptTags.js "./${1}/index.html" "./Play/${1}.html"
+echo "Compiling ${1}"
+./compile.js "./${1}/index.html" "./${1}.html"
 
